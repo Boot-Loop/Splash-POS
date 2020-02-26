380774ee9449bd582d7f11ce1c1a3c30435dc962
JesheemArafath <jesheemarafath@gmail.com>
2020-02-26 19:44:36 +0530

    Layout Changes

---
+++ POS-UI/src/views/home_views/Home.xaml.cs
@@ -0,0 +1,33 @@
+﻿using POS_UI.src.views.customers_views;
+using POS_UI.src.views.new_sale_views;
+using POS_UI.src.views.reports_views;
+using POS_UI.src.views.setting_views;
+using POS_UI.src.views.suppliers_views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS_UI.src.views.home_views
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Window
+    {
+        public Home()
+        {
+            InitializeComponent();
+            main_content_control.Content = new NewSale();
+        }
+    }
+}
