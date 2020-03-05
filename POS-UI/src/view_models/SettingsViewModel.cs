c5477a8b00d9a7b387e0554f7cb650064d3ad612
Azeem Muzammil <azeemmuzammil@yahoo.com>
2020-03-05 18:34:26 +0530

    settings created

---
+++ POS-UI/src/view_models/SettingsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UI.ViewModels.Commands;
+using UI.Views;
+
+namespace UI.ViewModels
+{
+    public class SettingsViewModel
+    {
+        public RelayCommand UserButtonCommand { get; private set; }
+        public HomeView HomeView { get; set; }
+
+        public SettingsViewModel(HomeView home_view) {
+            this.UserButtonCommand = new RelayCommand(userButtonPressed);
+            this.HomeView = home_view;
+        }
+
+        private void userButtonPressed(object parameter) {
+            HomeView.home_content_control.Content = new Users();
+        }
+    }
+}
