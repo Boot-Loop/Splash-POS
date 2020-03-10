b0c39773127cfba11fa91095eab50196be9d175f
JesheemArafath <jesheemarafath@gmail.com>
2020-03-10 22:05:37 +0530

    Edit & delete button work finished

--- POS-UI/src/views/stock_views/Stocks.xaml.cs
+++ POS-UI/src/views/stock_views/Stocks.xaml.cs
@@ -27,5 +27,19 @@ namespace UI.Views
             this._stock_view_model = new StockViewModel(home_view_model);
             this.DataContext = _stock_view_model;
         }
+
+        private void edit_button_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (edit_button.IsEnabled)
+                edit_button_image.Source = new BitmapImage(new Uri("/res/icons/edit.png", UriKind.Relative));
+            else edit_button_image.Source = new BitmapImage(new Uri("/res/icons/editdisable.png", UriKind.Relative));
+        }
+
+        private void delete_button_IsEnabledChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (delete_button.IsEnabled)
+                delete_button_image.Source = new BitmapImage(new Uri("/res/icons/bin.png", UriKind.Relative));
+            else delete_button_image.Source = new BitmapImage(new Uri("/res/icons/deletedisable.png", UriKind.Relative));
+        }
     }
 }
