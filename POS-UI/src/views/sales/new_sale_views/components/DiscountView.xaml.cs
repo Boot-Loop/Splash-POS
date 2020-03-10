907efdd85829e91478c1c10dd9e8e6d5c6134ffd
JesheemArafath <jesheemarafath@gmail.com>
2020-03-11 02:06:23 +0530

    discount view edits

--- POS-UI/src/views/sales/new_sale_views/components/DiscountView.xaml.cs
+++ POS-UI/src/views/sales/new_sale_views/components/DiscountView.xaml.cs
@@ -27,5 +27,51 @@ namespace UI.Views
         private void windowDeactivated(object sender, EventArgs e) {
             this.Close();
         }
+
+        private void btnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string name = button.Name;
+            if (name == "btnBS")
+            {
+                string text_result = inputTextBox.Text;
+                if (string.IsNullOrEmpty(inputTextBox.SelectedText))
+                {
+                    if (text_result.Length > 1)
+                    {
+                        text_result = text_result.Substring(0, text_result.Length - 1);
+                    }
+                    else
+                    {
+                        text_result = "";
+                    }
+                    inputTextBox.Text = text_result;
+                }
+                else
+                {
+                    inputTextBox.Text = "";
+                }
+            }
+            else if (name == "btnEsc")
+            {
+                this.Close();
+            }
+            else if (name == "btnEnter")
+            {
+                //enterPressed(Quantity);
+            }
+            else if (name == "btnDot")
+            {
+                if (string.IsNullOrEmpty(inputTextBox.SelectedText)) { if (!inputTextBox.Text.Contains(".")) { inputTextBox.AppendText("."); } }
+                else { inputTextBox.Text = "."; }
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(inputTextBox.SelectedText)) { inputTextBox.AppendText(Convert.ToString(button.Tag)); }
+                else { inputTextBox.Text = Convert.ToString(button.Tag); }
+            }
+            inputTextBox.CaretIndex = inputTextBox.Text.Length;
+        }
+
     }
 }
